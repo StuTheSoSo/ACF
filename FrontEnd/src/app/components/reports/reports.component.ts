4903901d289e7d2bcd0962678b96e6148b4a5dd3
stu.schwartz@gmail.com <stu.schwartz@gmail.com>
2025-08-27 14:39:22 -0400

    About to add Angular Material for tables

--- FrontEnd/src/app/components/reports/reports.component.ts
+++ FrontEnd/src/app/components/reports/reports.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent {
+  reportTypes: string[] = ['Case Reports', 'Usage Reports'];
+  selectedReportType: string = '';
+
+  reportTypeSelected(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedReportType = selectElement.value;
+  }
 
 }
